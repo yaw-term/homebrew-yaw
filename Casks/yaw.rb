e3c62a64c1ee68294471c22b0a810318c0709385
jef <yo@tkjef.com>
2026-02-25 11:37:39 -0800

    Update cask to arm64 build for 0.9.52

--- Casks/yaw.rb
+++ Casks/yaw.rb
@@ -1,8 +1,8 @@
 cask "yaw" do
   version "0.9.52"
-  sha256 "ac6651d18bbe0381fbef6f2e194cebd7cced21f1bceaed0c4f7c86e99eff0b7a"
+  sha256 "c3d9731b2d3918e62c62cea2278eacd3884dc74cd3ea30f87b54bbf565fe79f3"
 
-  url "https://yaw.sh/downloads/yaw-darwin-x64-#{version}.zip"
+  url "https://yaw.sh/downloads/yaw-darwin-arm64-#{version}.zip"
   name "yaw"
   desc "A modern terminal with built-in connection management and AI assistance"
   homepage "https://yaw.sh"
