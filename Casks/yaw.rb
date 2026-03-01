470fac91224642acbfb9af778c3ba5942a0df934
tkjef <yo@tkjef.com>
2026-03-01 14:24:15 -0800

    Bump yaw cask to v0.9.75

--- Casks/yaw.rb
+++ Casks/yaw.rb
@@ -1,6 +1,6 @@
 cask "yaw" do
-  version "0.9.74"
-  sha256 "d03b29f1c73a22d2f430b62650e5701dec0395e248e0482997d45a34daf4ff8c"
+  version "0.9.75"
+  sha256 "410685dd817c7a6a76f9aac1b3190c39e8821ff4f9c705b1f6b27f3de412bf22"
 
   url "https://yaw.sh/downloads/yaw-darwin-arm64-#{version}.zip"
   name "yaw"
