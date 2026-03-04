998a6ce242447d7c998143d2b7db02ad2f5cc6e0
tkjef <yo@tkjef.com>
2026-03-03 19:21:37 -0800

    Update macOS sha256 for v0.9.77 editMenu fix

--- Casks/yaw.rb
+++ Casks/yaw.rb
@@ -1,6 +1,6 @@
 cask "yaw" do
   version "0.9.77"
-  sha256 "870c12cc948f72ebe1ef501d400a63547be1b40ecbed3401bf88f868372272e5"
+  sha256 "60635ae86abd21db97e1a4a258f049f997007a43f0fa8e038a325f7fa0ecbc72"
 
   url "https://yaw.sh/downloads/yaw-darwin-arm64-#{version}.zip"
   name "yaw"
